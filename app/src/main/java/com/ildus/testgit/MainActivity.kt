b335407ffda8aac1c4410842397c6df1bf3a14e4
kildus <kadyrovildus@mail.ru>
2021-11-16 22:42:14 +0300

    commit_3

--- app/src/main/java/com/ildus/testgit/MainActivity.kt
+++ app/src/main/java/com/ildus/testgit/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "onCreate: 1")
+        Log.d(TAG, "onCreate: 3")
 
     }
 }
